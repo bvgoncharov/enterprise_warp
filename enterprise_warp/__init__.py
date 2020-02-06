25e7b04a725a74d150805519350e12a6eeee09b2
Boris Goncharov <goncharov.boris@physics.msu.ru>
2020-02-06 16:54:43 +1100

    Added tempo2_warp: get best noise realizations from tempo2

--- enterprise_warp/__init__.py
+++ enterprise_warp/__init__.py
@@ -2,4 +2,4 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-from . import enterprise_warp, libstempo_warp, bilby_warp
+from . import enterprise_warp, libstempo_warp, bilby_warp, tempo2_warp
---
+++ enterprise_warp/tempo2_warp.py
@@ -0,0 +1,35 @@
+import subprocess
+import sys
+
+def get_tempo2_prediction(par,tim,configuration,output_file):
+  """
+  Runs tempo2 and returns noise reconstruction in a file.
+  Plugin "general2" is used.
+  Configuration is a string in a string, example:
+    '"{bat}\t{freq}\t{post}\t{err}\t{posttn}\t{tndm}\t{tnrn}\n"'.
+  See tempo2 manual online for more information.
+  Output can be loaded with numpy.loadtxt().
+  """
+  
+  command = ['tempo2','-output','general2','-f',par,tim,'-s',configuration]
+
+  try:
+    result = subprocess.check_output(command)
+  except subprocess.CalledProcessError as t2_run_exception:
+    try:
+      # If the error is from "Too many TOAs" of tempo2
+      print('get_tempo2_prediction: handing tempo2 exception')
+      command.append('-nobs')
+      command.append('1000000')
+      result = subprocess.check_output(command)
+    except subprocess.CalledProcessError as ee:
+      print('Unknown error when running tempo2. Exiting.')
+      sys.exit(1)
+  result = result.decode("utf-8") # bytes to string
+  # Remove output junk before and after the actual output:
+  result = result.partition('Starting general2 plugin')[2]
+  result = result.partition('Finished general2 plugin')[0]
+  result = result.replace('"','') # remove quotation marks
+  
+  with open(output_file, "w") as output: output.write(result)
+
