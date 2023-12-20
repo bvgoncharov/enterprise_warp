1db50864c7499727486c323816356f35b6af9989
Boris Goncharov <goncharov.boris@physics.msu.ru>
2023-12-21 04:34:06 +1100

    Docstrings test

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+# -- Mocking libstempo installation (it can not be built because of tempo2) --
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['libstempo', 'other_module']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
