fbe67ff856c6bf204e310c6ffeb9682026b4b883
Boris Goncharov <goncharov.boris@physics.msu.ru>
2023-12-21 21:22:10 +1100

    Docstrings test 6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['libstempo', 'other_module']
+MOCK_MODULES = ['libstempo', 'scikit-sparse', 'other-dependency']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
